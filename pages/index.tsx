8750564fecf940a050e896a2e79a0ee8234233a1
ToniRealp <tonirealp@gmail.com>
2021-12-10 17:20:23 +0100

    Tailwind configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1> Toni Realp </h1>;
+    return <h1 className="text-3xl"> Toni Realp </h1>;
 };
 
 export default Home;
