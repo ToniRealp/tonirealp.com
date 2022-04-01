3af38f47148bc37824aac7ecf2c1ca3a224ac07c
ToniRealp <tonirealp@gmail.com>
2022-04-01 23:16:58 +0200

    Updated graphcms media url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 module.exports = {
     reactStrictMode: true,
     images: {
-        domains: ["media.graphcms.com"],
+        domains: [
+            "media.graphcms.com",
+            "media.graphassets.com"
+        ],
     },
     webpack: (config) => {
         config.module.rules.push({
